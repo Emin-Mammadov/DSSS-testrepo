e4efa276879cd9e87cdad37f3b28bf19318793d0
Emin Mammadov <eminmammadov@Emins-MacBook-Air.local>
2022-12-04 11:37:44 +0100

    adding all the necessary files for python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='snowflake',
+    version='0.1',
+    author='DSSS',
+    packages=find_packages(),
+    install_requires=['numpy','turtles'],
+)
